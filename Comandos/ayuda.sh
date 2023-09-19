e63cc5cb47ac078e6910b17eb56f0d701c910775
ivancanoo19 <ivan.fernandez@ingenieria.unam.edu}>
2023-09-19 08:59:30 -0600

    [ADD][CANO][MAIN] Agregué el comado ayuda

---
+++ Comandos/ayuda.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo
+echo
+echo Veo que necesitas un poco de ayuda...
+echo A continuación se muestra la lista de comandos personalizados disponibles :D
+echo
+
+echo "     COMANDO      |    DESCRIPCIÓN    "
+echo " ayuda            |  Muestra la lista de comandos disponibles en la terminal"
+echo " infosis          |  Muestra información básica del sistema operativo"
+echo " buscar           |  Busca un archivo a partir de la ruta del fichero en el que se encuentra"
+echo " creditos         |  Creditos al programados :D"
+echo " juego            |  Diversion..."
+echo " musica           |  Reproductor de musiquita"
+echo " salir            |  Salir del programa"
+
+
+echo
+echo
