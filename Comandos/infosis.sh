594581e8301f34e4c430a0de9805f1b01638a3b5
ivancanoo19 <ivan.fernandez@ingenieria.unam.edu}>
2023-09-19 08:49:29 -0600

    [ADD][CANO][MAIN] Agregué informacion del sistema en infosis

--- Comandos/infosis.sh
+++ Comandos/infosis.sh
@@ -8,10 +8,11 @@ echo Sistema operativo:
 cat /etc/issue
 
 echo Arquitectura:
-echo $HOSTTYPE
+arch
 
     #aux=$(cat /proc/meminfo | head -n 1 | grep [0-9])
     #echo $aux
+
 ram=$(cat /proc/meminfo | head -n 1)
 printf "\nRAM " $ram
 echo $ram
