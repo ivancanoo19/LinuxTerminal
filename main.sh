fe7f0e44f44547eee263ca6cc773458941bd4206
ivancanoo19 <ivan.fernandez@ingenieria.unam.edu}>
2023-09-21 10:46:33 -0600

    [MOD][CANO][MAIN] Modifique el titulo con formato bonito

--- main.sh
+++ main.sh
@@ -10,8 +10,24 @@ misComandos=(
     "salir"
 )
 
-comando="a  "
+title(){
+    echo
+    echo -e "\e[36m================================================================================"
+    echo -e "    __    _____   ____  ___  __    ________________  __  ________   _____    __ "
+    echo -e "   / /   /  _/ | / / / / / |/ /   /_  __/ ____/ __ \/  |/  /  _/ | / /   |  / / "
+    echo -e "  / /    / //  |/ / / / /|   /     / / / __/ / /_/ / /|_/ // //  |/ / /| | / /  "
+    echo -e " / /____/ // /|  / /_/ //   |     / / / /___/ _, _/ /  / // // /|  / ___ |/ /___"
+    echo -e "/_____/___/_/ |_/\____//_/|_|    /_/ /_____/_/ |_/_/  /_/___/_/ |_/_/  |_/_____/"
 
+    echo
+    echo -e "================================================================================\e[0m"  
+
+    echo
+    echo                         
+}
+
+comando="a  "
+title
 
 while ! [[ $comando =~ salir ]]; do
     #echo $USER : $PWD
@@ -23,6 +39,7 @@ while ! [[ $comando =~ salir ]]; do
     if grep -q "$comando" <<< "${misComandos[@]}"; then
         case $comando in
         ayuda)
+            title
             ./Comandos/ayuda.sh;;
         infosis)
             ./Comandos/infosis.sh;;
