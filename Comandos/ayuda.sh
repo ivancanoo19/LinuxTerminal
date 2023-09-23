59e0a61c3cb8fe4ef63d549cabe57f9c4c1a46e7
ivancanoo19 <ivan.fernandez@ingenieria.unam.edu}>
2023-09-22 19:41:08 -0600

    [FEATURE] Comando ayuda terminado

--- Comandos/ayuda.sh
+++ Comandos/ayuda.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-echo
+title(){
+    echo
+    echo -e "\e[36m=============================="
+    echo -e "    _____  ____  ______  ___ "
+    echo -e "   /   \ \/ / / / / __ \/   |"
+    echo -e "  / /| |\  / / / / / / / /| |"
+    echo -e " / ___ |/ / /_/ / /_/ / ___ |"
+    echo -e "/_/  |_/_/\____/_____/_/  |_|"
+    echo
+    echo -e "==============================\e[0m"  
+  
+    echo
+    echo
+}
+
+title
+
 echo
 echo Veo que necesitas un poco de ayuda...
 echo A continuación se muestra la lista de comandos personalizados disponibles :D
@@ -10,7 +26,7 @@ echo "     COMANDO      |    DESCRIPCIÓN    "
 echo " ayuda            |  Muestra la lista de comandos disponibles en la terminal"
 echo " infosis          |  Muestra información básica del sistema operativo"
 echo " buscar           |  Busca un archivo a partir de la ruta del fichero en el que se encuentra"
-echo " creditos         |  Creditos al programados :D"
+echo " creditos         |  Creditos al programador :D"
 echo " juego            |  Diversion..."
 echo " musica           |  Reproductor de musiquita"
 echo " salir            |  Salir del programa"
